74a70c63201f5c886f29343f95b58b895e44eaa3
John Doe <john.doe@gmail.com>
2019-07-10 08:56:49 -0300

    Commit git aula 1

--- ConsoleApp2/Properties/AssemblyInfo.cs
+++ ConsoleApp2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("ConsoleApp2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("ConsoleApp2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21bd2f35-6ae4-4b7a-a194-ba4d9870c777")]
+[assembly: Guid("cac7f333-36fe-4f13-9d69-aa0b85675ee1")]
 
 // Version information for an assembly consists of the following four values:
 //

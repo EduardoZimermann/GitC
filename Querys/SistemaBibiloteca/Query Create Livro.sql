54163a79e22a6c1a9c91d3a5368b12502fbfde48
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 17:25:15 -0300

    Database bibliotecas

---
+++ Querys/SistemaBibiloteca/Query Create Livro.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Livro] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Registro]    INT            NOT NULL,
+    [Titulo]      VARCHAR (1200) NOT NULL,
+    [Isbn]        VARCHAR (15)   NOT NULL,
+    [Genero]      INT            NOT NULL,
+    [Editora]     INT            NOT NULL,
+    [Sinopse]     NVARCHAR (MAX) NULL,
+    [Observacoes] VARCHAR (1000) NULL,
+    [Ativo]       BIT            NOT NULL,
+    [UsuInc]      INT            NOT NULL,
+    [UsuAlt]      INT            NOT NULL,
+    [DatInc]      DATETIME       DEFAULT (getdate()) NOT NULL,
+    [DatAlt]      DATETIME       DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Livro_Genero] FOREIGN KEY ([Genero]) REFERENCES [dbo].[Generos] ([Id]),
+    CONSTRAINT [FK_Livro_Editora] FOREIGN KEY ([Editora]) REFERENCES [dbo].[Editoras] ([Id]),
+    CONSTRAINT [FK_Livro_UsuarioAlt] FOREIGN KEY ([UsuAlt]) REFERENCES [dbo].[Usuarios] ([Id]),
+    CONSTRAINT [FK_Livro_UsuarioInc] FOREIGN KEY ([UsuInc]) REFERENCES [dbo].[Usuarios] ([Id])
+);
+
---
+++ Querys/SistemaBibiloteca/Query Create LivroAutor.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[LivroAutor]
+(
+    [Livro] INT NOT NULL, 
+    [Autor] INT NOT NULL, 
+
+    CONSTRAINT [FK_LivroAutor_Livro] 
+	FOREIGN KEY ([Livro]) 
+	REFERENCES [Livro]([Id]), 
+
+    CONSTRAINT [FK_LivroAutor_Autores] 
+	FOREIGN KEY ([Autor]) 
+	REFERENCES [Autores]([Id])
+)

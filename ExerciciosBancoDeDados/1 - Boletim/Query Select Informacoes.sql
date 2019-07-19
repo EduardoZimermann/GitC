368ef898012320b2ac520209d49a5a0200fb7322
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 15:16:42 -0300

    Sistema final final

--- ExerciciosBancoDeDados/1 - Boletim/Query Select Informacoes.sql
+++ ExerciciosBancoDeDados/1 - Boletim/Query Select Informacoes.sql
@@ -1,9 +1,10 @@
-﻿select
+﻿
+select
 		AL.Nome,
 		AL.Idade,
 		TU.Serie as 'Turma',
 		SUM(RE.Nota) / 4 as 'Media',
-		SUM(CASE RE.Presente WHEN 1 THEN 1 ELSE 0 END) * 100 / 4 as 'Frequencia',
+		SUM(CASE RE.Presente WHEN 1 THEN 1 ELSE 0 END) * 100 / COUNT(RE.Presente) as 'Frequencia',
 		IIF((SUM(RE.Nota) / 4) >= 7 and (SUM(CASE RE.Presente WHEN 1 THEN 1 ELSE 0 END) * 100 / 4) >= 75,'Aprovado','Reprovado') as 'Situação'
 	from Turmas TU
 	inner join Alunos AL on TU.Id = AL.Turma

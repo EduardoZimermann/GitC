d31c66b75805d0b37c00c1d434aac3572a4b3027
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 14:58:25 -0300

    Sistema boletim versão final

--- ExerciciosBancoDeDados/1 - Boletim/Query Create Notas.sql
+++ ExerciciosBancoDeDados/1 - Boletim/Query Create Notas.sql
@@ -1,10 +1,8 @@
 ﻿CREATE TABLE [dbo].[Registros] (
     [Id]    INT IDENTITY (1, 1) NOT NULL,
     [Aluno] INT NOT NULL,
-    [Nota1] INT NULL,
-    [Nota2] INT NULL,
-    [Nota3] INT NULL,
-    [Nota4] INT NULL,
+    [Nota] FLOAT NOT NULL,
+    [Presente] BIT NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Registros_Aluno] FOREIGN KEY ([Aluno]) REFERENCES [dbo].[Alunos] ([Id])
 );
---
+++ ExerciciosBancoDeDados/1 - Boletim/Query Select Informacoes.sql
@@ -0,0 +1,11 @@
+﻿select
+		AL.Nome,
+		AL.Idade,
+		TU.Serie as 'Turma',
+		SUM(RE.Nota) / 4 as 'Media',
+		SUM(CASE RE.Presente WHEN 1 THEN 1 ELSE 0 END) * 100 / 4 as 'Frequencia',
+		IIF((SUM(RE.Nota) / 4) >= 7 and (SUM(CASE RE.Presente WHEN 1 THEN 1 ELSE 0 END) * 100 / 4) >= 75,'Aprovado','Reprovado') as 'Situação'
+	from Turmas TU
+	inner join Alunos AL on TU.Id = AL.Turma
+	inner join Registros RE on AL.Id = RE.Aluno
+	group by AL.Nome,AL.Idade,TU.Serie

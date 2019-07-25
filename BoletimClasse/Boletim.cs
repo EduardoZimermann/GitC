5adf9260b0f763c2e7a739c5a0f182df17887701
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 08:34:56 -0300

    Exercício Classe

---
+++ BoletimClasse/Boletim.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletimClasse
+{
+    class Boletim
+    {
+        public double CalcularMedia(double nota1, double nota2, double nota3)
+        {
+            return (nota1 + nota2 + nota3) / 3;
+        }
+
+        public int CalcularFrequencia(int aulas, int faltas)
+        {
+            return ((aulas - faltas) * 100) / aulas;
+        }
+
+        public void MostrarInformacoes(double media, int frequencia)
+        {
+            Console.Clear();
+            Console.WriteLine($"Média: {string.Format("{0:0.00}", media)}");
+            Console.WriteLine($"Frequência: {frequencia}%");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BoletimClasse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletimClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boletim aluno = new Boletim();
+
+            Console.WriteLine("Informe as 3 notas do aluno:");
+
+            var media = aluno.CalcularMedia(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe o total de aulas e o número de faltas do aluno:");
+
+            var frequencia = aluno.CalcularFrequencia(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+
+            aluno.MostrarInformacoes(media, frequencia);
+        }
+    }
+}

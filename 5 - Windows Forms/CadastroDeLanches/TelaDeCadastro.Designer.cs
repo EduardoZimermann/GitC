0b2607981304da6423f02259ca6dafaa70939768
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-30 10:48:57 -0300

    Cadastro com forms

---
+++ 5 - Windows Forms/CadastroDeLanches/TelaDeCadastro.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CadastroDeLanches
+{
+    partial class TelaDeCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbxNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nrQuant = new System.Windows.Forms.NumericUpDown();
+            this.tbxValor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nrQuant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // tbxNome
+            // 
+            this.tbxNome.Location = new System.Drawing.Point(73, 15);
+            this.tbxNome.Name = "tbxNome";
+            this.tbxNome.Size = new System.Drawing.Size(304, 22);
+            this.tbxNome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantidade:";
+            // 
+            // nrQuant
+            // 
+            this.nrQuant.Location = new System.Drawing.Point(110, 50);
+            this.nrQuant.Name = "nrQuant";
+            this.nrQuant.Size = new System.Drawing.Size(267, 22);
+            this.nrQuant.TabIndex = 4;
+            // 
+            // tbxValor
+            // 
+            this.tbxValor.Location = new System.Drawing.Point(91, 83);
+            this.tbxValor.Name = "tbxValor";
+            this.tbxValor.Size = new System.Drawing.Size(286, 22);
+            this.tbxValor.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor: R$";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nome:";
+            // 
+            // TelaDeCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(396, 204);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nrQuant);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxNome);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaDeCadastro";
+            this.Text = "TelaDeCadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.nrQuant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nrQuant;
+        private System.Windows.Forms.TextBox tbxValor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
